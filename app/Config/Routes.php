ff3c6ce5477d0f6e3596366f1e3f8132faa37fa0
h0ag <technologie.ma3l@gmail.com>
2026-04-06 13:28:00 +0000

    Added auth routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -7,4 +7,10 @@ use CodeIgniter\Router\RouteCollection;
  */
 $routes->get('/', 'Home::index');
 $routes->get("/product/", "Home::product");
-$routes->get('/login', 'Users::login');
+$routes->get('/login', 'Users::login_page');
+
+/////// OAUTH ///////
+$routes->get('auth/login',    'Users::login');
+$routes->get('auth/callback', 'Users::callback');
+$routes->get('auth/logout',   'Users::logout');
+$routes->get('/getinfos', "Users::infos");
