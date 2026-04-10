10f88afe1c0e677f2ea9e8f82f885fc061b0af6c
h0ag <technologie.ma3l@gmail.com>
2026-04-10 17:47:55 +0000

    Added product base methods (home: send best items ; product: send actual product details ; item_thumbnail: to get sized item specific

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,11 +6,53 @@ class Home extends BaseController
 {
     public function index():string
     {
-        return $this->twig->render("home");
+        $bestsellers = $this->productModel->select('id, name, flavor, price, reference')->where('is_best_seller', true)->findAll();
+        $data = [
+            'best_sellers' => $bestsellers
+        ];
+        return $this->twig->render("home", $data);
     }
 
-    public function product():string
+    /**
+     * Returns the item's thumbnail as a resized WebP image.
+     *
+     * This method retrieves a item's thumbnail from the database, optionally resizes it,
+     * and outputs it directly as a WebP image. The 'size' query parameter can be used
+     * to request a custom dimension, with a maximum limit of 400 pixels. The default
+     * size is 150 pixels.
+     *
+     * @param int $itemID The ID of the item whose thumbnail is requested.
+     * @return void Outputs the image directly and terminates the script.
+     */
+    public function item_thumbnail($itemID, $real)
     {
-        return $this->twig->render("product");
+        $size = min((int) ($this->request->getGet("size") ?? 150), 400);
+
+        if($real == "0") {    
+            $thumbnail = $this->productModel->select('image_pixel_base64')->where('id', $itemID)->first()['image_pixel_base64'];
+        } else {
+            $thumbnail = $this->productModel->select('image_realistic_base64')->where('id', $itemID)->first()['image_realistic_base64'];
+        }
+
+        if (str_starts_with($thumbnail, "data:image/")) {
+            $thumbnail = explode(",", $thumbnail)[1];
+        }
+
+        $imgData = base64_decode($thumbnail);
+
+        $resizedImage = $this->productModel->resize_image($imgData, $size, $size);
+
+        header("Content-Type: image/webp");
+        echo $resizedImage;
+        exit();
+    }
+
+    public function product($itemID):string
+    {
+        $item = $this->productModel->select('id, name, stock_status, price, reference')->where('reference', $itemID)->first();
+        $data = [
+            'item' => $item
+        ];
+        return $this->twig->render("product", $data);
     }
 }
